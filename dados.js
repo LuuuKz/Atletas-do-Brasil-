78482ae79201c27adb8af9403c371aa6d214d104
LuuuKz <luizfernando.mk@hotmail.com>
2024-09-08 14:26:29 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -11,29 +11,4 @@ let dados = [
       link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
       tags: "judô judo judoca judoka"
   },
-  {
-      titulo: "Rebeca Andrade",
-      descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
-      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
-      tags: "ginástica ginastica ouro"
-  }
-];
-
-{
-    titulo: "Neymar Jr.",
-    descricao: "Neymar Jr. é um dos jogadores de futebol mais famosos do mundo, conhecido por seus dribles habilidosos e gols espetaculares. Atuando pelo Brasil e pelo Paris Saint-Germain, ele encanta fãs por onde passa.",
-    link: "https://pt.wikipedia.org/wiki/Neymar",
-    tags: "futebol neymar psg brasil" 
-  },
-  {
-    titulo: "Gabriel Medina",
-    descricao: "Gabriel Medina é um dos maiores nomes do surf mundial, tricampeão mundial e conhecido por seu estilo arrojado e radical. Sua paixão pelo esporte o levou a quebrar barreiras e inspirar uma nova geração de surfistas.",
-    link: "https://pt.wikipedia.org/wiki/Gabriel_Medina",
-    tags: "surf tricampeão mundial brasil"
-  },
-  {
-    titulo: "Ítalo Ferreira",
-    descricao: "Ítalo Ferreira é um fenômeno do surf brasileiro, campeão olímpico e mundialmente reconhecido por sua técnica agressiva e habilidade em ondas tubulares. Sua história de superação e conquista inspira muitos jovens.",
-    link: "https://pt.wikipedia.org/wiki/%C3%8Dtalo_Ferreira",
-    tags: "surf ouro olímpico brasil"
-  }
+  
